85324c05c49d4d9e99c72da1efa14d26e15c1355
Gabriel Rehbein <g.rehbein.rio@gmail.com>
2021-09-26 16:21:00 -0300

    beer test first steps

---
+++ tests/beer_test.go
@@ -0,0 +1,15 @@
+package tests
+
+import "github.com/cucumber/godog"
+
+func aGamblerInTheDBWithBets() error {
+	return godog.ErrPending
+}
+
+func theGamblerNeedsToSaveABet() error {
+	return godog.ErrPending
+}
+
+func theGamblerSavesHerhisBet() error {
+	return godog.ErrPending
+}
